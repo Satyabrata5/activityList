2f422cfaf49b7ee4f7e04eca2fb54cf1dc287dad
Satyabrata Nag <satyabrata.nag.career@gmail.com>
2019-07-08 12:12:14 +0530

    updated

--- force-app/main/default/aura/triggerQuickAction/triggerQuickActionController.js
+++ force-app/main/default/aura/triggerQuickAction/triggerQuickActionController.js
@@ -33,5 +33,53 @@
             .catch(function (error) {
                 console.log(error);
             });
+    },
+    associateUsages: function (component, event, helper) {
+        var url = '/apex/generatePDF?id=' + component.get("v.recordId");
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+            "url": url
+        });
+        urlEvent.fire();
+
+    },
+    savePDF: function (cmp, event, helper) {
+        var action = cmp.get("c.savePDFOpportunity");
+        action.setParams({
+            "caseId": cmp.get("v.recordId")
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            cmp.set('v.contentId', response.getReturnValue());
+            if (state === "SUCCESS") {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Success!",
+                    type : 'success',
+                    "message": "PDF generated successfully! Please check attachments"
+                });
+                toastEvent.fire();
+                $A.get('e.lightning:openFiles').fire({
+                    recordIds: [cmp.get("v.contentId")]
+                });
+                $A.get('e.force:refreshView').fire();
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                            errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
+
 })
